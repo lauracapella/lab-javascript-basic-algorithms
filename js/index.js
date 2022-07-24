f4d689ff99892c609382eaec0e21b737434ad9be
lauracapella <laura@laguapacomunicacion.com>
2022-07-24 10:16:14 +0200

    names and comate lenght of names

--- js/index.js
+++ js/index.js
@@ -1,6 +1,27 @@
+
+console.log("I'm ready!");
 // Iteration 1: Names and Input
-//
+
+let hacker1 = "Laura";
+
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2 = "David"
+
+console.log(`The navigator's name is ${hacker2}`)
+
 // Iteration 2: Conditionals
+
+let countNames1 = hacker1.length;
+let countNames2 = hacker2.length;
+
+if ( countNames1 > countNames2){
+    console.log(`The driver has the longest name, it has ${countNames1} characters.`)
+} else if(countNames1 > countNames2){
+    console.log(`It seems that the navigator has the longest name, it has ${countNames2} characters.`)
+}else{
+    console.log(`Wow, you both have equally long names, ${countNames1} characters!`)
+}
 
 
 // Iteration 3: Loops
